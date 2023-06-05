088ceedeca1489b32df6bb65415cb3cb5c1f76cd
Lae Chen <laec@proton.me>
2023-06-05 17:57:09 +1200

    Add file arg.

--- main.go
+++ main.go
@@ -11,9 +11,16 @@ import (
 func main() {
 	sortByName := flag.Bool("sort-by-name", false, "sort by name")
 	sortByValue := flag.String("sort-by-value", "", "sort by value")
+	path := flag.String("file", "-", "file")
 	flag.Parse()
 
-	input, err := io.ReadAll(os.Stdin)
+	var input []byte
+	var err error
+	if *path == "-" {
+		input, err = io.ReadAll(os.Stdin)
+	} else {
+		input, err = os.ReadFile(*path)
+	}
 	if err != nil {
 		if _, err := os.Stderr.WriteString(err.Error()); err != nil {
 			panic(err)
@@ -32,10 +39,15 @@ func main() {
 	if *sortByName {
 		node.SortByName()
 	}
-
 	if *sortByValue != "" {
 		node.SortByValue(*sortByValue)
 	}
 
-	println(node.String())
+	if *path == "-" {
+		println(node.String())
+	} else {
+		if err := os.WriteFile(*path, []byte(node.String()+"\n"), 0666); err != nil {
+			panic(err)
+		}
+	}
 }
